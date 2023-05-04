feabed8e5b69c386b5f9f617684d87de3d0fd570
Sany-SHiriCE <Sany-SHiriCE@302D-10>
2023-05-04 15:33:56 +0700

    Добавьте файлы проекта.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise2//Лабораторная работа по Мищенко №5(задание2)
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Check();
+        }
+        public class Book
+        {
+            public string Title { get; set; }
+        }
+        public static void Check()
+        {
+            var book = new Book();
+            book.Title = "Structure and interpretation of computer programs";
+            Console.WriteLine(book.Title);
+        }
+    }
+}
